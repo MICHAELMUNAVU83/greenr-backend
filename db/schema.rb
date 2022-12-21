f81a92d32bc0979c674531fc2558791dba57a554
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-12-21 18:39:02 +0300

    Created users

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_082448) do
     t.string "username"
     t.string "password_digest"
     t.string "email"
+    t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
