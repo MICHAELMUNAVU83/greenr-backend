a294eb7b8df05382da9fdda89674dfe8719a4f65
benaiah-ke <benaiahpraise@gmail.com>
2022-12-29 14:33:15 +0300

    add carbon credit create action

--- app/controllers/api/v1/carbon_credits_controller.rb
+++ app/controllers/api/v1/carbon_credits_controller.rb
@@ -4,4 +4,15 @@ class Api::V1::CarbonCreditsController < ApplicationController
         render json: CarbonCredit.all, status: :ok
     end
 
+    def create
+        carbon_credit = CarbonCredit.create(carbon_credit_params)
+        render json: carbon_credit, status: :created
+    end
+
+    private
+
+    def carbon_credit_params
+        params.require(:carbon_credit).permit(:amount, :price, :source, :image, :user_id)
+    end
+
 end
